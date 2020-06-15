d752ade46d170c3d56117387bc217e5017ee3b01
hugoboc <hugoboc@gmail.com>
2020-06-15 23:28:40 +0200

    adjusted esteps for extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -601,7 +601,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 200, 282 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 200, 805.68 }
 
 /**
  * Default Max Feed Rate (mm/s)
